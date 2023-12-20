9e8a05d0d9c328042201d311798fa50dc92b375b
0dminnimda <0dminnimda@gmail.com>
2023-12-20 22:59:54 +0300

    buff_uart\uart_rx: check for start and end bit

--- buff_uart/uart_rx.sv
+++ buff_uart/uart_rx.sv
@@ -1,12 +1,10 @@
 `include "if/uart_rx_if.sv"
 
-// check for start and end bit
-
 module uart_rx (uart_rx_if.DUT rx_if, input logic clock, logic resetn);
     localparam ticks_per_bit = rx_if.clock_freq / rx_if.baud_rate;
     localparam half_ticks_per_bit = rx_if.clock_freq / rx_if.baud_rate / 2;
 
-    enum logic [1:0] { WAIT_FOR_START_BIT, RECEIVING_DATA, RECEIVING_END } state;
+    enum logic [1:0] { WAIT_FOR_START_BIT, RECEIVING_START, RECEIVING_DATA, RECEIVING_END } state;
 
     logic [rx_if.width-1:0] bits;
     logic [$clog2(rx_if.width):0] bit_count;
@@ -15,8 +13,8 @@ module uart_rx (uart_rx_if.DUT rx_if, input logic clock, logic resetn);
     logic action;
     assign action = ticks_until_next_action <= 0;
 
-    logic signal;
-    assign signal = rx_if.signal == 0 && rx_if.can_receive_next_word;
+    logic signal_is_low;
+    assign signal_is_low = rx_if.signal == 0 && rx_if.can_receive_next_word;
 
     logic reading_the_last_bit;
     assign reading_the_last_bit = bit_count >= rx_if.width - 1;
@@ -24,12 +22,13 @@ module uart_rx (uart_rx_if.DUT rx_if, input logic clock, logic resetn);
     always_ff @(posedge clock) begin
         if (action) case(state)
             WAIT_FOR_START_BIT: begin
-                if (signal) begin
-                    ticks_until_next_action <= ticks_per_bit + half_ticks_per_bit;
+                if (signal_is_low) begin
+                    ticks_until_next_action <= half_ticks_per_bit;
                 end else begin
                     ticks_until_next_action <= 0;
                 end
             end
+            RECEIVING_START: ticks_until_next_action <= ticks_per_bit;
             RECEIVING_DATA: ticks_until_next_action <= ticks_per_bit;
             RECEIVING_END: ticks_until_next_action <= ticks_per_bit;
             default: ticks_until_next_action <= 0;
@@ -44,7 +43,7 @@ module uart_rx (uart_rx_if.DUT rx_if, input logic clock, logic resetn);
 
     always_ff @(posedge clock) begin
         if (action) case(state)
-            WAIT_FOR_START_BIT: if (signal) bit_count <= 0;
+            WAIT_FOR_START_BIT: if (signal_is_low) bit_count <= 0;
             RECEIVING_DATA: bit_count <= bit_count + 1;
         endcase
     end
@@ -59,7 +58,7 @@ module uart_rx (uart_rx_if.DUT rx_if, input logic clock, logic resetn);
 
     always_ff @(posedge clock) begin
         if (action) case(state)
-            RECEIVING_END: rx_if.data <= bits;
+            RECEIVING_END: if (!signal_is_low) rx_if.data <= bits;
         endcase
     end
 
@@ -67,7 +66,10 @@ module uart_rx (uart_rx_if.DUT rx_if, input logic clock, logic resetn);
         if (!resetn) begin
             state <= WAIT_FOR_START_BIT;
         end else if (action) case(state)
-            WAIT_FOR_START_BIT: if (signal) state <= RECEIVING_DATA;
+            WAIT_FOR_START_BIT: if (signal_is_low) state <= RECEIVING_START;
+            RECEIVING_START:
+                if (signal_is_low) state <= RECEIVING_DATA;
+                else state <= WAIT_FOR_START_BIT;
             RECEIVING_DATA: if (reading_the_last_bit) state <= RECEIVING_END;
             RECEIVING_END: state <= WAIT_FOR_START_BIT;
             default: state <= WAIT_FOR_START_BIT;
@@ -81,7 +83,7 @@ module uart_rx (uart_rx_if.DUT rx_if, input logic clock, logic resetn);
             rx_if.ready <= 0;
         end else if (action) case(state)
             WAIT_FOR_START_BIT: rx_if.ready <= 0;
-            RECEIVING_END: rx_if.ready <= 1;
+            RECEIVING_END: if (!signal_is_low) rx_if.ready <= 1;
         endcase
     end
 endmodule
