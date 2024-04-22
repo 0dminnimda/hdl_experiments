53854a689f4b8ba475ce1b8f51fdf6d57f155059
0dminnimda <0dminnimda@gmail.com>
2024-04-22 15:33:13 +0300

    Add questa_license

---
+++ questa_license/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LM_LICENSE_FILE=$(realpath ~/intelFPGA_pro/23.4/license/LR-166687_License.dat)
+# ~/intelFPGA_pro/23.4/questa_fse/bin/$@
+~/intelFPGA/23.1std/questa_fse/bin/$@
---
+++ questa_license/run_vlog.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+
+$(dirname $0)/run.sh vlog -reportprogress 300 -work $1 $2 -sv
