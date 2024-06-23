bdf796013e434c853757ff9e8e1960ba4961c78b
0dminnimda <0dminnimda@gmail.com>
2024-06-23 18:06:14 +0300

    questa_license/run.sh: allow to run on windows

--- questa_license/run.sh
+++ questa_license/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-export LM_LICENSE_FILE=$(realpath ~/intelFPGA_pro/23.4/license/LR-166687_License.dat)
-# ~/intelFPGA_pro/23.4/questa_fse/bin/$@
-~/intelFPGA/23.1std/questa_fse/bin/$@
+if [[ "$OSTYPE" == "msys" ]]; then
+  QUESTA="/c/intelFPGA/23.1std"
+  LICENSE="$QUESTA/license/LR-176611_License.dat"
+  BIN="$QUESTA/questa_fse/win64"
+else
+  QUESTA="$(realpath ~/intelFPGA/23.1std)"
+  LICENSE="$(realpath ~/intelFPGA_pro/23.4/license/LR-166687_License.dat)"
+  BIN="$QUESTA/questa_fse/bin"
+fi
+
+export LM_LICENSE_FILE="${LICENSE}"
+${BIN}/$@
+
