40319fa182250532412c61411ac2f64bbe231393
0dminnimda <0dminnimda@gmail.com>
2024-07-24 18:13:04 +0300

    Fix run.sh to work better

--- questa_license/run.sh
+++ questa_license/run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "msys" ]]; then
-  QUESTA="/c/intelFPGA/23.1std"
-  LICENSE="$QUESTA/license/LR-176611_License.dat"
-  BIN="$QUESTA/questa_fse/win64"
+    QUESTA="/c/intelFPGA/23.1std"
+    BIN="$QUESTA/questa_fse/win64"
 else
-  QUESTA="$(realpath ~/intelFPGA/23.1std)"
-  LICENSE="$(realpath ~/intelFPGA_pro/23.4/license/LR-166687_License.dat)"
-  BIN="$QUESTA/questa_fse/bin"
+    QUESTA="$(realpath ~/intelFPGA/23.1std)"
+    BIN="$QUESTA/questa_fse/bin"
 fi
+
+OLD_LICENSE=$(find "~/intelFPGA_pro/23.4/license" -name 'LR-*_License.dat')
+if [ $? -eq 0 ]; then
+    OLD_NAME=$(basename $OLD_LICENSE)
+    cp $OLD_LICENSE "$QUESTA/license/$OLD_NAME"
+fi
+
+LICENSE=$(find "$QUESTA/license/" -name 'LR-*_License.dat')
+echo "Using $LICENSE"
 
 export LM_LICENSE_FILE="${LICENSE}"
 ${BIN}/$@
